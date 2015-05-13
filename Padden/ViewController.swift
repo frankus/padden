6f374fc6199a97bb111b1d02d4a3e06ae6063eaf
Frank Schmitt <frank@frankschmitt.org>
2015-05-12 20:12:56 -0700

    Initial Commit

---
+++ Padden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Padden
+//
+//  Created by Frank Schmitt on 2015-05-12.
+//  Copyright (c) 2015 Frank Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
